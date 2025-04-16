024c2fbd9e14c40559545d4038665e14c504a971
Kunal Bagad <kunalbagad2004@gmail.com>
2025-04-16 12:54:55 +0530

    Update package.jsx

--- package.jsx
+++ package.jsx
@@ -1,17 +1,20 @@
 {
-    "name"; "shopify-amazon-importer",
-    "version"; "1.0.0",
-    "private"; true,
-    "description"; "A Shopify app to import Amazon products with affiliate and dropshipping support.",
-    "scripts"; {
-      "dev"; "concurrently \"npm run dev --prefix server\" \"npm start --prefix client\"",
-      "build"; "npm run build --prefix client",
-      "start"; "npm start --prefix server"
-    }
-    "dependencies"; {
-      "concurrently"; "^8.2.0"
-    }
-    "author"; "Your Name",
-    "license"; "MIT"
+  "name": "shopify-amazon-importer",
+  "version": "1.0.0",
+  "private": true,
+  "description": "A Shopify app to import Amazon products with affiliate and dropshipping support.",
+  "scripts": {
+    "dev": "concurrently \"npm run dev --prefix server\" \"npm start --prefix client\"",
+    "build": "cd client && npm install && npm run build",
+    "start": "cd server && npm install && npm start",
+    "postinstall": "cd client && npm install && cd ../server && npm install"
+  },
+  "dependencies": {
+    "concurrently": "^8.2.0"
+  },
+  "author": "Your Name",
+  "license": "MIT",
+  "engines": {
+    "node": ">=16.0.0"
   }
-  
+}
